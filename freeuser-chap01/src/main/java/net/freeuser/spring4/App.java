e6850e14b0fce52beb7874faa46be332ad56729d
freeuserkim <distance1218@gmail.com>
2017-04-09 17:43:58 +0900

    222

--- freeuser-chap01/src/main/java/net/freeuser/spring4/App.java
+++ freeuser-chap01/src/main/java/net/freeuser/spring4/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 왜 이런겨 ?????????>>>>>>>>>>>>>>>" );
+        System.out.println("Hello World! 왜 이런겨 ?????????>>>>>>>>>>>>>>>");
         System.out.println("111");
         
         
